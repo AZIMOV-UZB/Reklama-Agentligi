d4b2449885e3e965299d8cf62c1cb51b24350b42
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2025-02-22 07:36:17 +0500

    azimov

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
 import Header from "@/components/header/component";
-import Carousel from '@/components/carusel/Carousel'
-import Footer from '@/components/footer/Footer'
-import Map from '@/components/map/Map'
-import  Shop from '@/components/shop/shop'
-import Jobs from "@/components/jobs/Jobs"
+import Carousel from "@/components/carusel/Carousel";
+import Footer from "@/components/footer/Footer";
+import Map from "@/components/map/Map";
+import Shop from "@/components/shop/Shop";
+import Jobs from "@/components/jobs/Jobs";
 export default function Home() {
   return (
-<>
-<Header/>
-<Carousel/>
-<div className=" container ">
-<p className="  text-5xl font-bold my-6 text-white">FATH PRINT</p>
-<p className=" title text-5xl font-bold my-6 text-white ">TASHQI REKLAMA AGENTLGI
-</p>
-<p className=" text-xl font-bold my-6 text-white">SHAXSIY BIZNESINGIZNI REKLAMA ORQALI RIVOJLANTIRING
-</p>
-</div>
-<Shop/>
-<Map/>
-<Jobs/>
-
-<Footer/>
-
-</>
+    <>
+      <Header />
+      <Carousel />
+      <div className="container">
+        <p className="text-5xl font-bold my-6 text-white">FATH PRINT</p>
+        <p className="title text-4xl font-bold my-6 text-white">
+          TASHQI REKLAMA AGENTLIGI
+        </p>
+        <p className="text-xl font-bold my-6 text-white">
+          SHAXSIY BIZNESINGIZNI REKLAMA ORQALI RIVOJLANTIRING
+        </p>
+      </div>
+      <Shop />
+      <Map />
+      <Jobs />
+      <Footer />
+    </>
   );
 }
